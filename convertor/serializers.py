4403f0d4905601d4f930e964122f3065c934742f
Jaxon <xudoyberdiyevjahon8@gmail.com>
2025-02-07 17:02:57 +0500

    added new service is csvtoexcel and added caching for user upload files

--- convertor/serializers.py
+++ convertor/serializers.py
@@ -13,6 +13,8 @@ class FileUploadSerializer(serializers.Serializer):
             return self.validate_pdf_file(attrs)
         elif file.name.endswith(".xls") or file.name.endswith(".xlsx"):
             return self.validate_excel_file(attrs)
+        elif file.name.endswith('.csv'):
+            return self.validate_csv_file(attrs)
 
     def validate_doc_file(self, attrs):
         file = attrs.get('file')
@@ -29,8 +31,15 @@ class FileUploadSerializer(serializers.Serializer):
     def validate_excel_file(self, attrs):
         file = attrs.get('file')
         if (file.name.endswith('.xls') or file.name.endswith(".xlsx")) == False:
-            raise serializers.ValidationError("Only .docx files are allowed.")
+            raise serializers.ValidationError("Only .xlsx  files are allowed.")
         return attrs
+
+    def validate_csv_file(self, attrs):
+        file = attrs.get('file')
+        if not (file.name.endswith('.csv')):
+            raise serializers.ValidationError("Only .csv  files are allowed.")
+        return attrs
+
 
 
 
