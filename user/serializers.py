03d29195a695bbabf86996decd18ec790fb13659
Jaxon <xudoyberdiyevjahon8@gmail.com>
2025-02-21 13:45:39 +0500

    Add LoginView and RegisterView , add serializer for them, so test them

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,35 @@
 
-from django.contrib.auth.models import User
-from rest_framework.serializers import Serializer
+from user.models import CustomUser
+from rest_framework import serializers
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'passowd':{'write_only':True}}
+
+    def validata_password(self, value):
+        if len(value) < 8 :
+            raise serializers.ValidationError(" The password must be greater than or equal to 8 characters.")
+        return value
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, allow_blank=True)
+    password = serializers.CharField(max_length=128, write_only=True, allow_blank=True)
+
+
+
+
 
 
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from user.serializers import RegisterSerializer, LoginSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User successfully registered ', 'data': serializer.validated_data}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+
+            user = authenticate(request=request, username=email, password=password)
+            if user is None:
+                return Response({"error": "username or password are not correct"}, status=status.HTTP_400_BAD_REQUEST)
+
+            tokens = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(tokens),
+                "access": str(tokens.access_token),
+                "message": "User successfully logged in"
+            })
+        return Response(serializer.errors,  status=status.HTTP_400_BAD_REQUEST)
+
+
